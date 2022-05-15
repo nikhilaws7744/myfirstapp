ac65760c6693f2b4633c12d34093549df59f2256
nikhilaws7744 <105602592+nikhilaws7744@users.noreply.github.com>
2022-05-15 23:11:57 +0530

    Update streamlit_app.py
    update the multi select statemnt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as s
 import pandas as p
 
-s.multiselect("pick some fruits",list(mylist))
+
 
 mylist=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+s.multiselect("pick some fruits",list(mylist))
 s.dataframe(mylist)
