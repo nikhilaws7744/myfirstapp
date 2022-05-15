e860a14eed629a570c64e2fdf54a614d7ff28abe
nikhilaws7744 <105602592+nikhilaws7744@users.noreply.github.com>
2022-05-15 23:22:28 +0530

    Update streamlit_app.py
    fruit_view=fruit_name.loc[fruit_selected]

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ mylist=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_m
 fruit_name=mylist.set_index('Fruit')
 
 fruit_selected = s.multiselect("pick some fruits",list(fruit_name.index),['Banana','Apple'])
-fruit_view=fruit_selected.loc[fruit_selected]
+fruit_view=fruit_name.loc[fruit_selected]
 s.dataframe(fruit_view)
