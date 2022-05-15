8331cc195df018ae8d9c32ebff4b31669d094260
nikhilaws7744 <105602592+nikhilaws7744@users.noreply.github.com>
2022-05-15 23:17:43 +0530

    Update streamlit_app.py
    added 
    fruit_name=mylist.set_index('Fruit')
    
    s.multiselect("pick some fruits",list(fruit_name.index))

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as p
 
 mylist=p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-fruit_name=list(mylist.index)
+fruit_name=mylist.set_index('Fruit')
 
-s.multiselect("pick some fruits",list(mylist.fruit_name))
+s.multiselect("pick some fruits",list(fruit_name.index))
 s.dataframe(mylist)
